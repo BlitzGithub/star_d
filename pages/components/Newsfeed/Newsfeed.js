bc00ea8e0ef00b5995321cb8f01172e0155b7fb7
Ayush Kumar <k.ayushprogramming@gmail.com>
2023-04-08 16:15:51 +0530

    minor adjustments

--- pages/components/Newsfeed/Newsfeed.js
+++ pages/components/Newsfeed/Newsfeed.js
@@ -6,25 +6,29 @@ const Newsfeed = ({ title, summary, media, link }) => {
   const titleShort = title.slice(0, 77) + "...";
   return (
     <>
-      <div className={styles.newsFeedPost}>
-        <div className={styles.newsFeedImg}>
-          <img src={media} alt="..." className={styles.image} />
+      <div className={styles.newsFeedPostWrapper}>
+        <div className={styles.newsFeedPost}>
+          <div className={styles.newsFeedImg}>
+            <img src={media} alt="..." className={styles.image} />
+          </div>
+          <p>
+            <strong>{title.length < 30 ? title : titleShort}</strong>
+            <br />
+            <br />
+            {summary.length < 300 ? summary : summaryShort}
+          </p>
         </div>
-        <p>
-          <strong>{title.length < 30 ? title : titleShort}</strong>
-          <br />
-          <br />
-          {summary.length < 300 ? summary : summaryShort}
-        </p>
+        <div className={styles.explore}>
+          <button
+            onClick={() => {
+              window.open(link, "_blank");
+            }}
+          >
+            {/* <i></i> */}
+            <span>READ MORE</span>
+          </button>
+        </div>
       </div>
-      <button
-        onClick={() => {
-          window.open(link, "_blank");
-        }}
-        className={styles.newsFeedBtn}
-      >
-        READ MORE
-      </button>
     </>
   );
 };
